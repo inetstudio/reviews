3b6373bf1da9b978f81e33db9e881f31dd16c176
Konstantin Bukin <bukin@inetstudio.ru>
2018-08-15 16:15:34 +0300

    Change bindings provider

--- entities/messages/src/Providers/MessagesBindingsServiceProvider.php
+++ entities/messages/src/Providers/MessagesBindingsServiceProvider.php
@@ -3,33 +3,47 @@
 namespace InetStudio\Reviews\Messages\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Contracts\Foundation\Application;
 
 /**
  * Class MessagesBindingsServiceProvider.
  */
 class MessagesBindingsServiceProvider extends ServiceProvider
 {
+    /**
+    * @var  bool
+    */
     protected $defer = true;
 
-    public $bindings = [];
-
     /**
-     * MessagesBindingsServiceProvider constructor.
-     *
-     * @param Application $app
-     */
-    public function __construct(Application $app)
-    {
-        parent::__construct($app);
-
-        $this->bindings = getPackageBindings(__DIR__.'/../Contracts');
-    }
+    * @var  array
+    */
+    public $bindings = [
+        'InetStudio\Reviews\Messages\Contracts\Events\Back\ModifyMessageEventContract' => 'InetStudio\Reviews\Messages\Events\Back\ModifyMessageEvent',
+        'InetStudio\Reviews\Messages\Contracts\Http\Controllers\Back\MessagesControllerContract' => 'InetStudio\Reviews\Messages\Http\Controllers\Back\MessagesController',
+        'InetStudio\Reviews\Messages\Contracts\Http\Controllers\Back\MessagesDataControllerContract' => 'InetStudio\Reviews\Messages\Http\Controllers\Back\MessagesDataController',
+        'InetStudio\Reviews\Messages\Contracts\Http\Controllers\Back\MessagesUtilityControllerContract' => 'InetStudio\Reviews\Messages\Http\Controllers\Back\MessagesUtilityController',
+        'InetStudio\Reviews\Messages\Contracts\Http\Requests\Back\SaveMessageRequestContract' => 'InetStudio\Reviews\Messages\Http\Requests\Back\SaveMessageRequest',
+        'InetStudio\Reviews\Messages\Contracts\Http\Responses\Back\Messages\DestroyResponseContract' => 'InetStudio\Reviews\Messages\Http\Responses\Back\Messages\DestroyResponse',
+        'InetStudio\Reviews\Messages\Contracts\Http\Responses\Back\Messages\FormResponseContract' => 'InetStudio\Reviews\Messages\Http\Responses\Back\Messages\FormResponse',
+        'InetStudio\Reviews\Messages\Contracts\Http\Responses\Back\Messages\IndexResponseContract' => 'InetStudio\Reviews\Messages\Http\Responses\Back\Messages\IndexResponse',
+        'InetStudio\Reviews\Messages\Contracts\Http\Responses\Back\Messages\SaveResponseContract' => 'InetStudio\Reviews\Messages\Http\Responses\Back\Messages\SaveResponse',
+        'InetStudio\Reviews\Messages\Contracts\Http\Responses\Back\Messages\ShowResponseContract' => 'InetStudio\Reviews\Messages\Http\Responses\Back\Messages\ShowResponse',
+        'InetStudio\Reviews\Messages\Contracts\Http\Responses\Back\Utility\SuggestionsResponseContract' => 'InetStudio\Reviews\Messages\Http\Responses\Back\Utility\SuggestionsResponse',
+        'InetStudio\Reviews\Messages\Contracts\Models\MessageModelContract' => 'InetStudio\Reviews\Messages\Models\MessageModel',
+        'InetStudio\Reviews\Messages\Contracts\Observers\MessageObserverContract' => 'InetStudio\Reviews\Messages\Observers\MessageObserver',
+        'InetStudio\Reviews\Messages\Contracts\Repositories\MessagesRepositoryContract' => 'InetStudio\Reviews\Messages\Repositories\MessagesRepository',
+        'InetStudio\Reviews\Messages\Contracts\Services\Back\MessagesDataTableServiceContract' => 'InetStudio\Reviews\Messages\Services\Back\MessagesDataTableService',
+        'InetStudio\Reviews\Messages\Contracts\Services\Back\MessagesObserverServiceContract' => 'InetStudio\Reviews\Messages\Services\Back\MessagesObserverService',
+        'InetStudio\Reviews\Messages\Contracts\Services\Back\MessagesServiceContract' => 'InetStudio\Reviews\Messages\Services\Back\MessagesService',
+        'InetStudio\Reviews\Messages\Contracts\Services\Front\MessagesServiceContract' => 'InetStudio\Reviews\Messages\Services\Front\MessagesService',
+        'InetStudio\Reviews\Messages\Contracts\Transformers\Back\MessageTransformerContract' => 'InetStudio\Reviews\Messages\Transformers\Back\MessageTransformer',
+        'InetStudio\Reviews\Messages\Contracts\Transformers\Back\SuggestionTransformerContract' => 'InetStudio\Reviews\Messages\Transformers\Back\SuggestionTransformer',
+    ];
 
     /**
      * Получить сервисы от провайдера.
      *
-     * @return array
+     * @return  array
      */
     public function provides()
     {
--- entities/sites/src/Providers/SitesBindingsServiceProvider.php
+++ entities/sites/src/Providers/SitesBindingsServiceProvider.php
@@ -3,33 +3,46 @@
 namespace InetStudio\Reviews\Sites\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Contracts\Foundation\Application;
 
 /**
  * Class SitesBindingsServiceProvider.
  */
 class SitesBindingsServiceProvider extends ServiceProvider
 {
+    /**
+    * @var  bool
+    */
     protected $defer = true;
 
-    public $bindings = [];
-
     /**
-     * SitesBindingsServiceProvider constructor.
-     *
-     * @param Application $app
-     */
-    public function __construct(Application $app)
-    {
-        parent::__construct($app);
-
-        $this->bindings = getPackageBindings(__DIR__.'/../Contracts');
-    }
+    * @var  array
+    */
+    public $bindings = [
+        'InetStudio\Reviews\Sites\Contracts\Events\Back\ModifySiteEventContract' => 'InetStudio\Reviews\Sites\Events\Back\ModifySiteEvent',
+        'InetStudio\Reviews\Sites\Contracts\Http\Controllers\Back\SitesControllerContract' => 'InetStudio\Reviews\Sites\Http\Controllers\Back\SitesController',
+        'InetStudio\Reviews\Sites\Contracts\Http\Controllers\Back\SitesDataControllerContract' => 'InetStudio\Reviews\Sites\Http\Controllers\Back\SitesDataController',
+        'InetStudio\Reviews\Sites\Contracts\Http\Controllers\Back\SitesUtilityControllerContract' => 'InetStudio\Reviews\Sites\Http\Controllers\Back\SitesUtilityController',
+        'InetStudio\Reviews\Sites\Contracts\Http\Requests\Back\SaveSiteRequestContract' => 'InetStudio\Reviews\Sites\Http\Requests\Back\SaveSiteRequest',
+        'InetStudio\Reviews\Sites\Contracts\Http\Responses\Back\Sites\DestroyResponseContract' => 'InetStudio\Reviews\Sites\Http\Responses\Back\Sites\DestroyResponse',
+        'InetStudio\Reviews\Sites\Contracts\Http\Responses\Back\Sites\FormResponseContract' => 'InetStudio\Reviews\Sites\Http\Responses\Back\Sites\FormResponse',
+        'InetStudio\Reviews\Sites\Contracts\Http\Responses\Back\Sites\IndexResponseContract' => 'InetStudio\Reviews\Sites\Http\Responses\Back\Sites\IndexResponse',
+        'InetStudio\Reviews\Sites\Contracts\Http\Responses\Back\Sites\SaveResponseContract' => 'InetStudio\Reviews\Sites\Http\Responses\Back\Sites\SaveResponse',
+        'InetStudio\Reviews\Sites\Contracts\Http\Responses\Back\Utility\SuggestionsResponseContract' => 'InetStudio\Reviews\Sites\Http\Responses\Back\Utility\SuggestionsResponse',
+        'InetStudio\Reviews\Sites\Contracts\Models\SiteModelContract' => 'InetStudio\Reviews\Sites\Models\SiteModel',
+        'InetStudio\Reviews\Sites\Contracts\Observers\SiteObserverContract' => 'InetStudio\Reviews\Sites\Observers\SiteObserver',
+        'InetStudio\Reviews\Sites\Contracts\Repositories\SitesRepositoryContract' => 'InetStudio\Reviews\Sites\Repositories\SitesRepository',
+        'InetStudio\Reviews\Sites\Contracts\Services\Back\SitesDataTableServiceContract' => 'InetStudio\Reviews\Sites\Services\Back\SitesDataTableService',
+        'InetStudio\Reviews\Sites\Contracts\Services\Back\SitesObserverServiceContract' => 'InetStudio\Reviews\Sites\Services\Back\SitesObserverService',
+        'InetStudio\Reviews\Sites\Contracts\Services\Back\SitesServiceContract' => 'InetStudio\Reviews\Sites\Services\Back\SitesService',
+        'InetStudio\Reviews\Sites\Contracts\Services\Front\SitesServiceContract' => 'InetStudio\Reviews\Sites\Services\Front\SitesService',
+        'InetStudio\Reviews\Sites\Contracts\Transformers\Back\SiteTransformerContract' => 'InetStudio\Reviews\Sites\Transformers\Back\SiteTransformer',
+        'InetStudio\Reviews\Sites\Contracts\Transformers\Back\SuggestionTransformerContract' => 'InetStudio\Reviews\Sites\Transformers\Back\SuggestionTransformer',
+    ];
 
     /**
      * Получить сервисы от провайдера.
      *
-     * @return array
+     * @return  array
      */
     public function provides()
     {
